44ed0fb359af01338ee693da172721da31011cfa
Dmytro Ushchenko <dmytro.ushchenko@vaimo.com>
2023-03-01 08:53:35 +0200

    sort strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,21 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  let sortedArray = [...arr];
+  let english = /^[A-Za-z0-9]*$/;
+  sortedArray.sort(function (a, b) {
+    if (english.test(a[0]) === true && english.test(b[0]) !== true) {
+      return 1;
+    }
+    if (a.toUpperCase() === b.toUpperCase()) {
+      return -1;
+    }
+    return a.localeCompare(b);
+  });
 
+  if (param === 'desc') {
+    sortedArray.reverse();
+  }
+
+  return sortedArray;
 }
