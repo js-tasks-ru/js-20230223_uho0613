ee0c7d9ebfc0ee3cd5ef451854d345efb454e1e7
Dmytro Ushchenko <dmytro.ushchenko@vaimo.com>
2023-03-12 19:26:31 +0200

    need resolve haw to render element

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
 
+  constructor(message = '', {duration = 1000, type = 'default'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.initElement();
+  }
+
+  initElement() {
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = this.defaultElement;
+    this.element = wrapper.firstElementChild;
+  }
+
+  show(targetElement = false) {
+    if (targetElement !== false) {
+      this.element = this.getUpdatedTemplate(targetElement);
+    }
+    console.log(this.element);
+    document.body.innerHTML += this.getMessageTemplate();
+    document.body.innerHTML += this.element;
+
+    setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
+
+  getUpdatedTemplate(targetElement) {
+    targetElement.classList.add(this.type);
+    targetElement.classList.add('notification-body');
+    targetElement.innerHTML = this.message;
+    return targetElement;
+  }
+
+  get defaultElement() {
+    return `<div class="notification-body ${this.type}">
+        ${this.message}
+      </div>`;
+  }
+
+  getMessageTemplate() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+    </div>
+  </div>`;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
